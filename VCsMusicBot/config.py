4afb11c02da0c2c8a4c5e1988a6c1858c7d15f3c
Alhaq143 <133550178+Alhaq143@users.noreply.github.com>
2023-07-17 13:19:37 +0530

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -7,16 +7,16 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "1BVtsOHYBu2WE6wvOg7Af3OuRAJUjhtzR7d8DeRNzp_Q2xrEk0znUR35FNSOoYYhg38_DAwPb-oyXJd2yGTZgYApm5Hz1QkfvsfH-8f24oP4slObnA9noWga-hEQEctdNM3nZLy_nhtndLgWEHtPdbUlND56qQXDuoV-IgGBWy0c1XxpJ2N4F_NhX5WACiaC6_ZYlLwYeOmXkuL3s5g9vgCQP34Nb4T7mHEutDFs32_7lhLJeTuHv9FXMKezVODIvi-YikFKxCGOKmj0EWfsRZMWquC7-W4M27erZquvVmEocvb19D4DbiCIoYXxSNjv_4sMGXCPnG9V7hH71KmYpyP17kOy2YvY=")
+BOT_TOKEN = getenv("6124433807:AAFLeTMZQGQ0W5xjkLQ3I_pQcDSuXGgY_Cs")
+BOT_NAME = getenv("ISLAMIC NASHEED")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
 admins = {}
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
+API_ID = int(getenv("API_ID", "27730948"))
+API_HASH = getenv("5a59be7f1753e4a0c21d3b68c89efaf8")
+BOT_USERNAME = getenv("@Islamic_nasheedbot")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "@Alhaq143")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "zauteschat")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
 SOURCE_CODE = getenv("SOURCE_CODE", "github.com/ZauteKm/VCsMusicBot")
